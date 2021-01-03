228ec6da6b6107d405c2121eaf7361760fb7ec14
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-03 16:01:08 +0000

    removes .pyi files which are unneeded

--- png.pyi
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from typing import Optional
-
-import numpy as np
-
-class Image:
-    def save(self, file: Path) -> None: ...
-
-def from_array(arr: np.ndarray, mode: Optional[str] = None) -> Image: ...
